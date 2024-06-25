287d0d4c43a7545c5c5bd617614631c0a3a06c6f
Uday Shah <udayshah107.ud@gmail.com>
2024-06-25 16:31:20 +0530

    Add files via upload

---
+++ Landing-Page/script.js
@@ -0,0 +1,7 @@
+function showMenu() {
+    document.getElementById('hamburger-menu-links').style.display='flex'
+}
+
+function hideMenu() {
+    document.getElementById('hamburger-menu-links').style.display='none'
+}
